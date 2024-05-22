364648abff430e616ecaa89702ffa83eeb557784
Daniel Andres <86764234+daancabo97@users.noreply.github.com>
2024-05-21 20:29:04 -0500

    Delete Leccion04 directory

--- Leccion04/ControlClientes/src/main/java/da/com/cb/dao/PersonaDao.java
+++
@@ -1,9 +0,0 @@
-package da.com.cb.dao;
-
-import da.com.cb.domain.Persona;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface PersonaDao extends CrudRepository<Persona, Long> {
-    
-}
