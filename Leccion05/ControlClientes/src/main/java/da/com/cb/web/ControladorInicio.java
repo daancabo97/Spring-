4497446a9760a4ef0245f24c6b700b693a78db8f
daancabo97 <danielcaicedo285@gmail.com>
2024-06-27 23:14:10 -0500

    Sección 5: Spring - Manejo Transaccional; Sección 6: Spring - CRUD con completo con Spring Boot y MySql

---
+++ Leccion05/ControlClientes/src/main/java/da/com/cb/web/ControladorInicio.java
@@ -0,0 +1,48 @@
+package da.com.cb.web;
+
+import lombok.extern.slf4j.Slf4j;
+import da.com.cb.domain.Persona;
+import da.com.cb.servicio.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+    
+    @Autowired
+    private PersonaService personaService;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        var personas = personaService.listarPersonas();
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+    
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+}
+
+/*
+ * http://localhost:8080/inicio
+ */
